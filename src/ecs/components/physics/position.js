fe743b4efaf75002e840b59bdc30db4dd6327f03
Alex Hoover <alexwhoover@gmail.com>
2026-03-01 09:34:18 +0000

    refactor: Combined Position and Collider components as they were highly coupled anyways

--- src/ecs/components/physics/position.js
+++ src/ecs/components/physics/position.js
@@ -1,6 +1,15 @@
 class Position {
-    constructor(x, y) {
+    constructor(x, y, width, height) {
+        // x and y are the center point of the entity
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    getBoundingBox() {
+        // Helper method to convert variables to AABB collision algorithm convention
+        // (left_x, top_y) is top left corner of bounding box
+        return {left_x: this.x - this.width / 2, top_y: this.y - this.height / 2, w: this.width, h: this.height };
     }
 }
--- src/ecs/systems/render_system.js
+++ src/ecs/systems/render_system.js
@@ -3,13 +3,12 @@ class RenderSystem extends System {
         super(ecs);
     }
     update() {
-        const entities = this.ecs.getEntitiesWith(Position, Collider, Renderable);
+        const entities = this.ecs.getEntitiesWith(Position, Renderable);
         for (let entity of entities) {
-            const collider = this.ecs.getComponent(entity, Collider);
             const pos = this.ecs.getComponent(entity, Position);
             const render = this.ecs.getComponent(entity, Renderable);
 
-            const bb = collider.getBoundingBox(pos);
+            const bb = pos.getBoundingBox();
             fill(...render.color);
             noStroke();
             rect(bb.left_x, bb.top_y, bb.w, bb.h);
