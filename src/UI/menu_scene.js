488512cffd19cb0572e50ece4ca78010c7dea4ce
Tony <s481597@gmail.com>
2026-03-02 21:13:09 +0000

    add: menu scene

---
+++ src/UI/menu_scene.js
@@ -0,0 +1,12 @@
+// menu_ui.js
+class MenuScene extends Scene {
+    constructor() {
+        super();  
+        this.items = ["New Game", "Settings", "Credits"];
+        this.selected = 0;
+    }
+  
+    display() {
+
+    }
+}
