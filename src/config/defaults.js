9f68a250bd1bd8f0192444ca14332ffba311d7ce
Alex Hoover <alexwhoover@gmail.com>
2026-03-12 20:49:27 +0000

    feat: Implemented multiple enemy types

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -12,11 +12,15 @@ const DEFAULTS = {
     },
     sizes: {
         player: { width: 40, height: 40 },
-        enemy:  { width: 20, height: 20 },
+        enemy:  { width: 30, height: 30 },
+        large_enemy: {width: 50, height: 50},
         box:    { width: 20, height: 20 },
+    },
+    health: {
+        player: 3,
+        enemy: 2,
+        large_enemy: 5
     },
     wallThickness: 20,
     hurtTime: 280,
-    playerHealth: 3,
-    enemyHealth: 5
 };
--- src/ecs/systems/enemy_spawn_system.js
+++ src/ecs/systems/enemy_spawn_system.js
@@ -1,3 +1,15 @@
+const EnemyType = Object.freeze({
+    NORMAL: "NORMAL",
+    LARGE: "LARGE",
+    FLOATING: "FLOATING"
+})
+
+const EnemyConfig = Object.freeze({
+    [EnemyType.NORMAL]: { entityType: EntityType.GROUND_ENEMY, width: DEFAULTS.sizes.enemy.width, height: DEFAULTS.sizes.enemy.height, health: DEFAULTS.health.enemy, color: [100, 10, 200] },
+    [EnemyType.LARGE]: { entityType: EntityType.GROUND_ENEMY, width: DEFAULTS.sizes.large_enemy.width, height: DEFAULTS.sizes.large_enemy.height, health: DEFAULTS.health.large_enemy, color: [80, 5, 160] },
+    [EnemyType.FLOATING]: { entityType: EntityType.FLOATING_ENEMY, width: DEFAULTS.sizes.enemy.width, height: DEFAULTS.sizes.enemy.height, health: DEFAULTS.health.enemy, color: [10, 200, 255]},
+});
+
 class EnemySpawnSystem extends System {
     constructor(ecs, spawner) {
         super(ecs);
@@ -14,13 +26,22 @@ class EnemySpawnSystem extends System {
         this.spawnTimer += dt;
 
         if (this.spawnTimer >= this.spawnRate) {
-            this.spawner.request(EntityType.ENEMY, {
-                center_x: 400,
-                center_y: 20,
-                width: DEFAULTS.sizes.enemy.width,
-                height: DEFAULTS.sizes.enemy.height
+            const enemyType = this.getRandomEnemyType();
+            const config = EnemyConfig[enemyType];
+            this.spawner.request(config.entityType, {
+                center_x: Math.round(window.innerWidth / 2),
+                center_y: 0,
+                width: config.width,
+                height: config.height,
+                health: config.health,
+                color: config.color
             })
             this.spawnTimer = 0;
         }
+    }
+
+    getRandomEnemyType() {
+        const enemyTypes = [EnemyType.NORMAL, EnemyType.LARGE, EnemyType.FLOATING];
+        return enemyTypes[Math.floor(Math.random() * enemyTypes.length)];
     }
 }
