b9507390a32dafe9294e5a8d47ec7993e3ffeccb
Alex Hoover <alexwhoover@gmail.com>
2026-04-06 22:28:23 +0100

    fix: Scaled projectile size as well

--- src/ecs/components/weapon.js
+++ src/ecs/components/weapon.js
@@ -7,9 +7,9 @@ const WeaponType = Object.freeze({
 const BASICRIFLE = {
     fireRate: 100, 
     bulletDamage: 1, 
-    bulletSpeed: 10, 
-    bulletSize: {w: 6, h: 4},
-    recoilKick: 0.003,
+    bulletSpeed: 0.2, 
+    bulletSize: {w: 0.12, h: 0.08},
+    recoilKick: 0.00006,
     maxRange: 0.3,
 
 }
@@ -19,9 +19,9 @@ const WEAPON_CONFIGS = {
   [WeaponType.TWOWAYRIFLE]: BASICRIFLE,
   [WeaponType.ROCKET]: {fireRate: 500, 
                         bulletDamage: 3, 
-                        bulletSpeed: 20, 
-                        bulletSize: {w:18, h:12},
-                        recoilKick: 0.009,
+                        bulletSpeed: 0.4, 
+                        bulletSize: {w:0.36, h:0.24},
+                        recoilKick: 0.00018,
                         maxRange: 0.7,
                       },
 }
@@ -32,13 +32,19 @@ class Weapon {
     if (type){
       const config = WEAPON_CONFIGS[type]
       this.lastShotTime = 0;
+
       // Map config data to the component instance
       this.fireRate = config.fireRate;
       this.bulletDamage = config.bulletDamage;
-      this.bulletSpeed = config. bulletSpeed;
-      this.bulletSize = config.bulletSize;
-      this.recoilKick = config.recoilKick;
       this.maxRange = config.maxRange;
+
+      this.bulletSpeed = LevelFactory.scaleX(config.bulletSpeed, width);
+      this.recoilKick  = LevelFactory.scaleX(config.recoilKick, width);
+
+      this.bulletSize = {
+          w: LevelFactory.scaleX(config.bulletSize.w, width),
+          h: LevelFactory.scaleY(config.bulletSize.h, height)
+      };
     }
   }
 }
