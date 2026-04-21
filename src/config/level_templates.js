028b6ec819036f00290cfb2c3099f3d082cde416
Alex Hoover <alexwhoover@gmail.com>
2026-04-21 14:38:59 +0100

    Added level 2

--- src/config/level_templates.js
+++ src/config/level_templates.js
@@ -7,14 +7,16 @@ const LevelTemplates = {
             { x0: 11, y0: 4, x1: 20, y1: 4, spawnable: true },
             { x0: 0, y0: -20, x1: 0, y1: 17, spawnable: false }, // Boundary: Left edge
             { x0: 31, y0: -20, x1: 31, y1: 17, spawnable: false }, // Boundary: Right edge
+            
             { x0: 3, y0: 7, x1: 6, y1: 7, spawnable: true },
             { x0: 25, y0: 7, x1: 28, y1: 7, spawnable: true },
             { x0: 9, y0: 11, x1: 12, y1: 11, spawnable: true },
             { x0: 19, y0: 11, x1: 22, y1: 11, spawnable: true },
             { x0: 3, y0: 14, x1: 6, y1: 14, spawnable: true },
             { x0: 25, y0: 14, x1: 28, y1: 14, spawnable: true },
-            { x0: 0, y0: 17, x1: 13, y1: 17, spawnable: false }, // Boundary: Bottom-left
-            { x0: 18, y0: 17, x1: 31, y1: 17, spawnable: false }, // Boundary: Bottom-right
+
+            { x0: 0, y0: 17, x1: 13, y1: 17, spawnable: true }, // Boundary: Bottom-left
+            { x0: 18, y0: 17, x1: 31, y1: 17, spawnable: true }, // Boundary: Bottom-right
         ]
     },
     2: {
@@ -27,16 +29,16 @@ const LevelTemplates = {
             { x0: 18, y0: -20, x1: 31, y1: 0, spawnable: false }, // Boundary: Top-right
             { x0: 0, y0: -20, x1: 0, y1: 17, spawnable: false }, // Boundary: Left edge
             { x0: 31, y0: -20, x1: 31, y1: 17, spawnable: false }, // Boundary: Right edge
-            { x0: 12, y0: 4, x1: 22, y1: 4, spawnable: true },
-            { x0: 9, y0: 7, x1: 13, y1: 7, spawnable: true },
-            { x0: 30, y0: 7, x1: 30, y1: 7, spawnable: true },
-            { x0: 5, y0: 10, x1: 9, y1: 10, spawnable: true },
-            { x0: 19, y0: 10, x1: 27, y1: 10, spawnable: true },
-            { x0: 1, y0: 13, x1: 1, y1: 13, spawnable: true },
-            { x0: 15, y0: 12, x1: 15, y1: 13, spawnable: true },
-            { x0: 25, y0: 14, x1: 30, y1: 14, spawnable: true },
-            { x0: 1, y0: 17, x1: 13, y1: 17, spawnable: false }, // Boundary: Bottom-left
-            { x0: 18, y0: 17, x1: 30, y1: 17, spawnable: false }, // Boundary: Bottom-right
+
+            { x0: 6, y0: 3, x1: 18, y1: 3, spawnable: true },
+            { x0: 19, y0: 6, x1: 25, y1: 6, spawnable: true },
+            { x0: 24, y0: 9, x1: 30, y1: 9, spawnable: true },
+            { x0: 1, y0: 7, x1: 7, y1: 7, spawnable: true },
+            { x0: 8, y0: 12, x1: 23, y1: 12, spawnable: true },
+            { x0: 14, y0: 9, x1: 16, y1: 9, spawnable: true },
+
+            { x0: 0, y0: 17, x1: 13, y1: 17, spawnable: true }, // Boundary: Bottom-left
+            { x0: 18, y0: 17, x1: 31, y1: 17, spawnable: true }, // Boundary: Bottom-right
         ]
     },
     3: {
@@ -54,7 +56,6 @@ const LevelTemplates = {
 
             { x0: 1, y0: 4, x1: 3, y1: 4, spawnable: true },
             { x0: 13, y0: 4, x1: 19, y1: 4, spawnable: true },
-            // { x0: 25, y0: 7, x1: 27, y1: 7, spawnable: true },
             { x0: 7, y0: 10, x1: 9, y1: 10, spawnable: true },
             { x0: 13, y0: 9, x1: 14, y1: 9, spawnable: true },
             { x0: 18, y0: 11, x1: 24, y1: 11, spawnable: true },
@@ -64,8 +65,8 @@ const LevelTemplates = {
             { x0: 24, y0: 4, x1: 24, y1: 6, spawnable: true },
 
 
-            { x0: 1, y0: 17, x1: 13, y1: 17, spawnable: false }, // Boundary: Bottom-left
-            { x0: 18, y0: 17, x1: 30, y1: 17, spawnable: false }, // Boundary: Bottom-right
+            { x0: 1, y0: 17, x1: 13, y1: 17, spawnable: true }, // Boundary: Bottom-left
+            { x0: 18, y0: 17, x1: 30, y1: 17, spawnable: true }, // Boundary: Bottom-right
         ]
     }
 };
